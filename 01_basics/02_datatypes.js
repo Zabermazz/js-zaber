63d054f9a90823cd08fde052dbbe7670b017eeb0
Zabermazz <147089110+Zabermazz@users.noreply.github.com>
2024-01-10 14:11:20 +0000

    new text updated

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,6 +1,16 @@
-"use strict"; // treat all Js code as newer version 
+// "use strict"; // treat all Js code as newer version 
 
 // alert("hello")
 // we are using node js not browser
 
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined =>
+// symbol => unique
+// object
 
+console.log(typeof undefined);
+console.log(typeof null);
