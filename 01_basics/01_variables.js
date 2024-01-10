c75456d878b0b37963a2a12a4d9c93d570f242bb
Zabermazz <147089110+Zabermazz@users.noreply.github.com>
2024-01-10 08:25:03 +0000

    newe file added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "zaber.mamzumder@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 not allowed 
+
+acoountEmail = "hc@hc.com"
+accountPassword = "12333"
+accountCity = "bangalore"
+let accountState;
+
+/*
+prefer not to use var
+because  of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+ 
+// console.log(accountId);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,6 @@
+"use strict"; // treat all Js code as newer version 
+
+// alert("hello")
+// we are using node js not browser
+
+
